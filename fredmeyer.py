fb9324256bfb4bcdb412fbfa99acddde34622688
trevorkw7 <54345106+trevorkw7@users.noreply.github.com>
2020-08-22 17:18:57 -0700

    make it better

--- fredmeyer.py
+++ fredmeyer.py
@@ -14,7 +14,7 @@ fourPack = "https://www.fredmeyer.com/p/clorox-disinfecting-wipes-multi-pack/000
 def checker (site, name):
 
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
-    response = requests.get(site, headers=headers, timeout=5)
+    response = requests.get(site, headers=headers)
     soup =  BeautifulSoup(response.content, "html.parser")
 
     ship_data = soup.find('label', attrs={"for": "DELIVERY"})
